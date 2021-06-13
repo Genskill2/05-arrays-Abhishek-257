ffbb3748e9cd316afb11239b8848e1c09fe5e66a
Abhishek-257 <67688464+Abhishek-257@users.noreply.github.com>
2021-06-13 14:14:05 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,16 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+int max(int x[],int y)
+{
+  int i,j,MAX;
+  i=0;
+  MAX=x[0];
+  for(j=i+1; j<y; j++)
+  {
+    if(x[j]>MAX){
+      MAX=x[j];
+    i++;
+    }
+  }
+    return MAX;
+}
