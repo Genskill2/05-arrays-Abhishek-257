bf81f5d75b8dc472a85e858bcb8b73179415a1b5
Abhishek-257 <67688464+Abhishek-257@users.noreply.github.com>
2021-06-15 10:14:36 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,5 +1,4 @@
-/* Enter your solutions in this file */
-#include <stdio.h>
+
 int max(int x[], int y);
 {
   int i,j,MAX;
@@ -14,5 +13,48 @@ int max(int x[], int y);
   }
     return MAX;
 }
+int mode(int a[], int n)
+{
+  int count,i,max=0,t[n];
+  for(i=0; i<n; i++) {
+    t[i]=0;
+    for(int j=0; j<n; j++){
+        if(a[j]==a[i])
+           t[i]++;
+    }
+     if(t[i] > max){
+     max=t[i];
+     count=i;
+     }
+   }
+   return a[count];
+}
+      
+      
+int min(int x[],int y)
+{
+  int i=0,j,MIN;
+
+  MIN=x[0];
+  for(j=i+1; j<y; j++)
+  {
+    if(x[j] < MIN){
+      MIN=x[j];
+      i++;
+    }
+  }
+    return MIN;
+}
+
+float average(int x[], int n)
+{
+  float avg,sum=0;
+  int i;
+  for(i=0; i<n; i++)
+    sum+=x[i];
+  avg=sum/n;
+  return avg;
+} 
+ 
 
 
