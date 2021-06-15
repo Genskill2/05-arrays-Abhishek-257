0fb889cc0e08ae382ca13e45294d6dd19cd17455
Abhishek-257 <67688464+Abhishek-257@users.noreply.github.com>
2021-06-15 09:58:55 +0530

    Update test_max.c

--- test_max.c
+++ test_max.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
 #include <assert.h>
 
-int max(int x[], int);
+
+int mode(int [], int);
 
 int main(void) {
-  int x[] = {9,5,6,10,2,-3,4};
-  int y=7;
- 
-  printf("%d",max(x, y));
-}
-int max(int x[],int y)
-{
-  int i,j,MAX;
-  i=0;
-  MAX=x[0];
-  for(j=i+1; j<y; j++)
-  {
-    if(x[j]>MAX){
-      MAX=x[j];
-    i++;
-    }
+  int x[] = {4, 9,5,6,5,10,0,2,-3, -3,4, 4};
+  assert ((mode(x, 12) == 4));
+
+  int y[] = {5};
+  assert (mode(y, 1) == 5);
+  printf("Mode: passed\n");
   }
-    return MAX;
-}
